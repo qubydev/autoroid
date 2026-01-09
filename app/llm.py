3ba2848bda0317286fd518b983f3e4ded3332459
malay77patra <malay77patra@gmail.com>
2026-01-09 18:56:50 +0530

    added ollama

--- app/llm.py
+++ app/llm.py
@@ -2,6 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
 from app.config import LLM_PROVIDER, BASE_URL, API_KEY, MODEL
+from langchain_ollama import ChatOllama
 
 def get_model():
     if LLM_PROVIDER == "openai":
@@ -22,6 +23,10 @@ def get_model():
             google_api_key=API_KEY,
             # Strict mode
             convert_system_message_to_human=False 
+        )
+    elif LLM_PROVIDER == "ollama":
+        return ChatOllama(
+            model=MODEL
         )
     else:
         raise ValueError(f"Unsupported LLM_PROVIDER: {LLM_PROVIDER}")
